270798fabe0270d785973e9714ff1e10423a4c1b
Rotar Andreea <andreearotar103@gmail.com>
2023-02-16 13:18:29 +0200

    added the design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Die from './Die';
 
 function App() {
+  const [value,setValue]=React.useState(allNewDice)
+  
+  function allNewDice(){
+    const array=[]
+    var i=11
+    while(i)
+    {
+      array.push(Math.floor(Math.random() * 7 + 1))
+      i--
+    }
+    return array
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+          <h1>Tenzies</h1>
+          <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls</p>
+          <div className='dice-container'>
+              <Die value={value[1]}/>
+              <Die value={value[2]}/>
+              <Die value={value[3]}/>
+              <Die value={value[4]}/>
+              <Die value={value[5]}/>
+              <Die value={value[6]}/>
+              <Die value={value[7]}/>
+              <Die value={value[8]}/>
+              <Die value={value[9]}/>
+              <Die value={value[10]}/>
+          </div>
+    </main>
   );
 }
-
+ 
 export default App;
---
+++ src/Die.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css';
+
+
+const Die = (props) => {
+  return (
+    <div className='box'>{props.value}</div>
+  )
+}
+
+export default Die
