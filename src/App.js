332c02c0c5550e4a384386bf79078b40d7df781d
Rotar Andreea <andreearotar103@gmail.com>
2023-02-16 14:06:11 +0200

    mapping the diece array and solving React update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import Die from './Die';
 
 function App() {
-  const [value,setValue]=React.useState(allNewDice)
+  const [dice,setDice]=React.useState(allNewDice())
   
+  const diceValue=dice.map(die=>
+      <Die
+          value={die}
+      />
+    )
+
   function allNewDice(){
     const array=[]
-    var i=11
+    var i=10
     while(i)
     {
       array.push(Math.floor(Math.random() * 7 + 1))
@@ -21,16 +27,7 @@ function App() {
           <h1>Tenzies</h1>
           <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls</p>
           <div className='dice-container'>
-              <Die value={value[1]}/>
-              <Die value={value[2]}/>
-              <Die value={value[3]}/>
-              <Die value={value[4]}/>
-              <Die value={value[5]}/>
-              <Die value={value[6]}/>
-              <Die value={value[7]}/>
-              <Die value={value[8]}/>
-              <Die value={value[9]}/>
-              <Die value={value[10]}/>
+              {diceValue}
           </div>
     </main>
   );
