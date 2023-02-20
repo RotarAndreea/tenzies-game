40c2d145472b766dd1c7c6388e077cedfa641b62
Rotar Andreea <andreearotar103@gmail.com>
2023-02-20 09:09:11 +0200

    end of the game

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './App.css';
 import Die from './Die';
 import { nanoid } from 'nanoid';
+import Confetti from 'react-confetti';
 
 function App(props) {
   const [dice,setDice]=React.useState(allNewDice() )
+  const [tenzies,setTenzies]=React.useState(false)
   
   const diceValue=dice.map(die=>
       <Die
@@ -54,9 +56,24 @@ function App(props) {
       
     }))    
   }
+  function reloadDice(){
+    setDice(allNewDice)
+    setTenzies(prevState => !prevState)
+  }
+
+  React.useEffect(()=>{
+    const allHeld = dice.every(die=> die.isHeld)
+    const lastValue= dice[9].value
+    const allSameValue=dice.every(die => die.value === lastValue)
+    if(allHeld && allSameValue){
+      setTenzies(true)
+      console.log("You won!")
+    }
+  },[dice])
 
   return (
     <main>
+          {tenzies && <Confetti />}
           <h1>Tenzies</h1>
           <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls</p>
           <div className='dice-container'>
@@ -64,9 +81,9 @@ function App(props) {
           </div>
           <button 
                 className='roll-button'
-                onClick={rollDice}
+                onClick={tenzies ? reloadDice  : rollDice} 
           >
-              Roll
+              {tenzies ? "Reload" : "Roll" }
           </button>
     </main>
   );
